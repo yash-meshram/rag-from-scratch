50c38481c4ea1622ff9d498c10e77a6f75c0b672
yash meshram <yashmeshram4102000@gmail.com>
2026-03-25 00:50:13 +0530

    added embedding

--- pdf_loader.py
+++ pdf_loader.py
@@ -56,3 +56,36 @@ def split_documents(documents, chunk_size = 1000, chunk_overlap = 200):
 all_pdf_docs = [page for doc in all_pdf_documents for page in doc]
 chunks = split_documents(documents = all_pdf_docs)
 
+
+# Embedding - convert text to vector
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+class EmbeddingManager():
+    def __init__(self, model_name: str = "multi-qa-MiniLM-L6-cos-v1"):
+        self.model_name = model_name
+        self.load_model()
+        self.model = None
+        
+    def load_model(self):
+        try:
+            print(f"Loading model {self.model_name} ...")
+            self.model = SentenceTransformer(self.model_name)
+            print(f"\nModel embedding: {self.model.get_sentence_embedding_dimension()}")
+            print(f"\nModel loaded successfully.")
+        except Exception as e:
+            print(f"Error loading model {self.model_name}: {e}")
+            raise
+        
+    def generate_embedding(self, texts: list[str]) -> np.array:
+        if not self.model:
+            raise ValueError("Model not loaded.")
+        print(f"Genrating embedding for {len(texts)} chunks...")
+        embeddings = self.model.encode(texts, batch_size=32, show_progress_bar=True)
+        print(f"Embedding generated, shape: {embeddings.shape}")
+        return embeddings
+    
+
+embedding_manager = EmbeddingManager()
+texts = [chunk.page_content for chunk in chunks]
+embeddings = embedding_manager.generate_embedding(texts)
